b90472a0790b9316ffae074fa7c7aa5f0b5fa2a9
WavuaW <wavuamuroka@gmail.com>
2024-10-07 13:57:35 +0300

    Adding the Portfolio, Services, Contact and Footer

--- Portfolio-Website-TailWind/app.js
+++ Portfolio-Website-TailWind/app.js
@@ -1,7 +1,15 @@
 const hamburger = document.querySelector("#hamburger")
 const menu = document.querySelector("#menu")
+const aLinks = document.querySelectorAll("#aLink")
 
 hamburger.addEventListener("click", ()=>{
     menu.classList.toggle("hidden")
     hamburger.classList.toggle("bg-white")
+})
+
+aLinks.forEach(link=>{
+    link.addEventListener("click", ()=>{
+        menu.classList.toggle("hidden")
+        hamburger.classList.toggle("bg-white")
+    })
 })
