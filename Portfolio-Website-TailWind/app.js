f71d8f65d8acfe9abeed48e3fb8d0070a0dfaa05
WavuaW <wavuamuroka@gmail.com>
2024-10-08 11:11:15 +0300

    Implementing dark mode

--- Portfolio-Website-TailWind/app.js
+++ Portfolio-Website-TailWind/app.js
@@ -1,5 +1,7 @@
 const hamburger = document.querySelector("#hamburger")
 const menu = document.querySelector("#menu")
+const moon = document.querySelector("#moon")
+const body = document.querySelector("body")
 const aLinks = document.querySelectorAll("#aLink")
 
 hamburger.addEventListener("click", ()=>{
@@ -13,3 +15,25 @@ aLinks.forEach(link=>{
         hamburger.classList.toggle("bg-white")
     })
 })
+
+moon.addEventListener("click", ()=>{
+    body.classList.toggle("dark")
+})
+
+// // Function to update the logo based on dark mode
+// function updateLogo() {
+//     const logo = document.getElementById("logo");
+//     const isDarkMode = document.documentElement.classList.contains("dark");
+  
+//     if (isDarkMode) {
+//       logo.src = "./img/logo-white.png";  // Replace with your dark mode logo
+//     } else {
+//       logo.src = "./img/logo2.png";  // Replace with your light mode logo
+//     }
+//   }
+  
+  
+//   // Optional: Listen for theme changes dynamically
+//   const observer = new MutationObserver(updateLogo);
+//   observer.observe(document.documentElement, { attributes: true, attributeFilter: ["class"] });
+  
